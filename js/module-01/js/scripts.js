0e2cf3d7c49af67023aadc500c16826382c5497f
boyardd <boyardd@gmail.com>
2018-04-22 10:42:26 +0300

    adds homework js

---
+++ js/module-01/js/scripts.js
@@ -0,0 +1,47 @@
+let sharm = 15;
+let hurgada = 25;
+let tuba = 6;
+
+let userTickets = prompt("Введите количество необходимых билетов:");
+const asNumber = Number(userTickets);
+
+const isValidInput = userTickets !== null && !Number.isNaN(asNumber);
+if (isValidInput) {
+
+    if (userTickets <= tuba){
+       const okTrip = confirm('Есть место в группе на Tuba, вы согласны?');
+        if (okTrip){
+            alert('Отлично вы в группе Tuba');
+            tuba = tuba - userTickets;
+        }
+        else {
+            alert('Нам жаль'); 
+        }
+    }
+
+    if (userTickets <= sharm){
+        const okTrip = confirm('Есть место в группе в Sharm, вы согласны?');
+         if (okTrip){
+             alert('Отлично вы в группе Sharm');
+             sharm = sharm - userTickets;
+         }
+         else {
+             alert('Нам жаль'); 
+         }
+     }
+
+     if (userTickets <= hurgada){
+        const okTrip = confirm('Есть место в группе на Tuba, вы согласны?');
+         if (okTrip){
+             alert('Отлично вы в группе Tuba');
+             hurgada = hurgada - userTickets;
+         }
+         else {
+             alert('Нам жаль'); 
+         }
+     }
+
+} else {
+  alert('Ошибка ввода');
+}
+
