b2e8cc063e1b360053e3627cabbc19b84026b8cf
boyardd <boyardd@gmail.com>
2018-05-21 12:50:06 +0300

    adds revision 02 to module-03

--- js/module-03/js/scripts.js
+++ js/module-03/js/scripts.js
@@ -1,18 +1,29 @@
 "use strict";
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-const login = "Mango";
+const login = "gMango";
 
+// const checkLoginValidity = function(login){
+//   let validLogin = login;
+//   if (validLogin.length >= 4 && validLogin.length <= 16){
+//     validLogin = true;
+//   }
+//  else {
+//      alert("Ошибка");
+//      validLogin = false;
+
+//  }
+//   console.log(validLogin);
+//   return validLogin;
+
+// }
 const checkLoginValidity = function(login) {
-  let validLogin = login;
-  if (validLogin.length >= 4 && validLogin.length <= 16) {
-    validLogin = true;
+  if (login.length >= 4 && login.length <= 16) {
+    return true;
   } else {
-    alert("Ошибка");
-    validLogin = false;
+    // alert ("Ошибка");
+    return false;
   }
-  console.log(validLogin);
-  return validLogin;
 };
 
 const checkIfLoginExists = function(logins, login) {
@@ -21,18 +32,33 @@ const checkIfLoginExists = function(logins, login) {
   let userLogin = login;
   for (let i = 0; i < arrLogins.length; i++) {
     if (userLogin.includes(arrLogins[i])) {
-      alert("Такой логин уже используется");
+      // alert("Такой логин уже используется")
+      return true;
     } else {
-      alert("Логин успешно добавлен");
-      userLogin.push(arrLogins[i]);
+      return false;
     }
+    // else{
+    //  alert("Логин успешно добавлен")
+    //  userLogin.push(arrLogins[i]);
+    // }
   }
 };
 
 const addLogin = function(logins, login) {
   let checkLogin;
+  let checkExists;
+  let userLogin = login;
   if ((checkLogin = checkLoginValidity(login))) {
-    console.log(checkIfLoginExists(logins, login));
+    checkIfLoginExists(logins, login);
+    if ((checkExists = checkIfLoginExists(logins, login))) {
+      alert("Такой логин уже используется");
+    } else {
+      alert("Логин успешно добавлен");
+      userLogin.push(arrLogins[i]);
+    }
+  } else {
+    alert("Ошибка");
   }
 };
+
 console.log(addLogin(logins, login));
