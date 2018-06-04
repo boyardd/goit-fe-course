1650f79a91d8592f48e1c5d3c17e6e49c622fd7d
boyardd <boyardd@gmail.com>
2018-06-04 21:09:14 +0300

    adds third revision  to module-03

--- js/module-03/js/scripts.js
+++ js/module-03/js/scripts.js
@@ -1,64 +1,42 @@
 "use strict";
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-const login = "gMango";
+const login = "uMango";
 
-// const checkLoginValidity = function(login){
-//   let validLogin = login;
-//   if (validLogin.length >= 4 && validLogin.length <= 16){
-//     validLogin = true;
-//   }
-//  else {
-//      alert("Ошибка");
-//      validLogin = false;
 
-//  }
-//   console.log(validLogin);
-//   return validLogin;
-
-// }
 const checkLoginValidity = function(login) {
   if (login.length >= 4 && login.length <= 16) {
     return true;
   } else {
-    // alert ("Ошибка");
     return false;
   }
 };
 
 const checkIfLoginExists = function(logins, login) {
-  let loginExist;
-  let arrLogins = logins;
-  let userLogin = login;
-  for (let i = 0; i < arrLogins.length; i++) {
-    if (userLogin.includes(arrLogins[i])) {
-      // alert("Такой логин уже используется")
-      return true;
-    } else {
-      return false;
-    }
-    // else{
-    //  alert("Логин успешно добавлен")
-    //  userLogin.push(arrLogins[i]);
-    // }
-  }
+  // let loginExist;
+  // let arrLogins = logins;
+  // let userLogin = login;
+  return logins.includes(login);
+ 
 };
 
 const addLogin = function(logins, login) {
-  let checkLogin;
-  let checkExists;
+  
   let userLogin = login;
-  if ((checkLogin = checkLoginValidity(login))) {
-    checkIfLoginExists(logins, login);
-    if ((checkExists = checkIfLoginExists(logins, login))) {
-      alert("Такой логин уже используется");
-    } else {
-      alert("Логин успешно добавлен");
-      userLogin.push(arrLogins[i]);
-    }
-  } else {
-    alert("Ошибка");
+  let arrLogins = logins;
+
+  const loginValid = checkLoginValidity(login);
+  if(!loginValid){
+    alert('Ошибка');
+    return;
   }
+  const loginExists = checkIfLoginExists(logins, login);
+  if(loginExists){
+    alert("Такой логин уже используется");
+    return;
+  }
+  arrLogins.push(userLogin);
+  alert('логин успешно добавлен');
 };
 
 console.log(addLogin(logins, login));
